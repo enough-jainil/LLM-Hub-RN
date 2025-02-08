2bea0803961d7abe25ade156c1c1141505c1b7e9
Jainil Prajapati 🪐 <github-jainil@1209.in>
2025-02-08 09:15:03 +0530

    Configure Babel for NativeWind and Expo preset
    - Update Babel configuration to use NativeWind with Expo preset
    - Remove module resolver plugin
    - Simplify Babel configuration for better compatibility with NativeWind

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@components': './components',
-            '@src': './src',
-            '~': './src'
-          }
-        }
-      ]
-    ]
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
-}; 
+};
